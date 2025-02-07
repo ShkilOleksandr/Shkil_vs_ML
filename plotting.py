ded907e441de94ac250fde701db9019cdef8a01a
OleksandrShkil <127298290+OleksandrShkil@users.noreply.github.com>
2025-02-07 12:17:09 +0100

    first 3 chapters

---
+++ plotting.py
@@ -0,0 +1,17 @@
+from nnfs.datasets import spiral_data
+import numpy as np
+import nnfs
+import matplotlib.pyplot as plt
+
+nnfs.init()
+
+
+
+#The start of the fun
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap='brg')
+
+plt.show()
