ecad5e19940c43791b5e0a263fa3e458a98a4ec7
OleksandrShkil <127298290+OleksandrShkil@users.noreply.github.com>
2025-02-08 20:17:04 +0100

    accuracy

--- main.py
+++ main.py
@@ -83,3 +83,10 @@ loss = loss_function.calculate(activation2.output, y)
 
 print('loss: ',loss)
 
+predictions = np.argmax(activation2.output, axis=1)
+
+if len(y.shape) == 2:
+    y =  np.argmax(y, axis=1)
+accuracy = np.mean(predictions == y)
+
+print('accuracy: ', accuracy)
